54fa04a9e603d819ccd28155fc6af864ee2c4530
Madelyn Cruz <mcruz42@calpoly.edu>
2023-01-25 12:11:04 -0800

    first commit

---
+++ numbers.js
@@ -0,0 +1,21 @@
+function div (a, b){
+  return a / b;
+}
+
+function containsNumbers(text){
+  for (let i = 0; i < text.length; i++) {
+   if (!isNaN(text.charAt(i)))
+    return true;
+  }
+  return false;
+}function div (a, b){
+  return a / b;
+}
+
+function containsNumbers(text){
+  for (let i = 0; i < text.length; i++) {
+   if (!isNaN(text.charAt(i)))
+    return true;
+  }
+  return false;
+}
